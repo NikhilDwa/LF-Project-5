7773636bb09730690b176928d0e7d4e5dbb392cc
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-22 19:46:18 +0545

    vgg16 function updated

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from src.resnet50 import *
+from src import resnet_model_score
+from src import vgg16_model_score
 
 image_1 = "./data/dog.jpg"
 image_2 = "./data/cat.jpg"
 
-similarity_score = resnet_model_score(image_1, image_2)
+vgg16_similarity_score = vgg16_model_score(image_1, image_2)
+resnet_similarity_score = resnet_model_score(image_1, image_2)
 
-print("The similarity score between the two input images is: ", similarity_score)
+print("The vgg16 similarity score between the two input images is: ", vgg16_similarity_score)
+print("The resnet similarity score between the two input images is: ", resnet_similarity_score)
+
