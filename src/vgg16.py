665acceaf68d809a5b68b3ff500268d2044a444f
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-22 20:55:30 +0545

    vgg16 function updated

--- src/vgg16.py
+++ src/vgg16.py
@@ -1,19 +1,14 @@
-import numpy as np
-from PIL import Image
 from keras.applications.vgg16 import VGG16
-from tensorflow.keras.preprocessing import image
-from sklearn.metrics.pairwise import cosine_similarity
-from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+from tensorflow.keras.applications.vgg16 import preprocess_input
+
+from .main import process_image, cosine_similarity_calculation
 
 model = VGG16(weights='imagenet')
 
 
 def vgg16_embedding(image_input):
 
-    input_image = Image.open(image_input)
-    image_resize = input_image.resize((224, 224))
-    image_array = image.img_to_array(image_resize)
-    image_array = np.expand_dims(image_array, axis=0)
+    image_array = process_image(image_input)
     image_preprocessed = preprocess_input(image_array)
     embedding = model.predict(image_preprocessed)
 
@@ -24,6 +19,6 @@ def vgg16_model_score(image_one, image_two):
 
     image_one_embedding = vgg16_embedding(image_one)
     image_two_embedding = vgg16_embedding(image_two)
-    cosine_score = cosine_similarity(image_one_embedding, image_two_embedding)
+    cosine_score = cosine_similarity_calculation(image_one_embedding, image_two_embedding)
 
     return cosine_score
