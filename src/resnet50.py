f507da74c83155edf07138be71a107ead7a2ba55
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-23 11:22:33 +0545

    ResNet function updated

--- src/resnet50.py
+++ src/resnet50.py
@@ -7,6 +7,14 @@ model = ResNet50(weights='imagenet')
 
 
 def resnet_embedding(image_input):
+    """
+        Takes image array and compute embedding using ResNet50 model.
+
+        Returns
+        ------------------------
+        [array]
+            [returns an image embedding.]
+    """
 
     image_array = process_image(image_input)
     image_preprocessed = preprocess_input(image_array)
@@ -16,6 +24,14 @@ def resnet_embedding(image_input):
 
 
 def resnet_model_score(image_one, image_two):
+    """
+        Takes two images, preprocess the image and find similarity score.
+
+        Returns
+        ------------------------
+        [float]
+            [returns similarity score between the image.]
+    """
 
     image_one_embedding = resnet_embedding(image_one)
     image_two_embedding = resnet_embedding(image_two)
