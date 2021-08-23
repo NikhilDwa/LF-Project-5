2fb5feed77d67b79ffa1e832d60bea20a048713a
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-23 10:44:48 +0545

    calculation of processed time

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from src import resnet_model_score
+import time
+
+from src import pca_model_score
 from src import vgg16_model_score
+from src import resnet_model_score
 
-image_1 = "./data/dog.jpg"
-image_2 = "./data/cat.jpg"
+image_1 = "./data/cat.jpg"
+image_2 = "./data/dog.jpg"
 
+vgg_start = time.perf_counter()
 vgg16_similarity_score = vgg16_model_score(image_1, image_2)
+vgg_end = time.perf_counter() - vgg_start
+
+resnet_start = time.perf_counter()
 resnet_similarity_score = resnet_model_score(image_1, image_2)
+resnet_end = time.perf_counter() - resnet_start
+
+pca_start = time.perf_counter()
+pca_similarity_score = pca_model_score(image_1, image_2)
+pca_end = time.perf_counter() - pca_start
 
 print("The vgg16 similarity score between the two input images is: ", vgg16_similarity_score)
 print("The resnet similarity score between the two input images is: ", resnet_similarity_score)
+print("Using PCA, the similarity score between the two input images is: ", pca_similarity_score)
 
+print("Time taken to compute VGG16 is :", vgg_end)
+print("Time taken to compute Resnet is :", resnet_end)
+print("Time taken to compute PCA is :", pca_end)
