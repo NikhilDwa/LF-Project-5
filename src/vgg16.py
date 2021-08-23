5390c29af47768ad575f5ad7c714ab967aa84fc0
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-23 11:21:45 +0545

    VGG16 function updated

--- src/vgg16.py
+++ src/vgg16.py
@@ -7,6 +7,14 @@ model = VGG16(weights='imagenet')
 
 
 def vgg16_embedding(image_input):
+    """
+        Takes image array and compute embedding using VGG16 model.
+
+        Returns
+        ------------------------
+        [array]
+            [returns an image embedding.]
+    """
 
     image_array = process_image(image_input)
     image_preprocessed = preprocess_input(image_array)
@@ -16,7 +24,14 @@ def vgg16_embedding(image_input):
 
 
 def vgg16_model_score(image_one, image_two):
+    """
+        Takes two images, preprocess the image and find similarity score.
 
+        Returns
+        ------------------------
+        [float]
+            [returns similarity score between the image.]
+    """
     image_one_embedding = vgg16_embedding(image_one)
     image_two_embedding = vgg16_embedding(image_two)
     cosine_score = cosine_similarity_calculation(image_one_embedding, image_two_embedding)
