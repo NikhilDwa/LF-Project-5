7e78b8e0c99fedd31ab546acbecf5f37ed711695
Nikhil Dwa <nikhildwa627@gmail.com>
2021-08-23 10:45:20 +0545

    addition of PCA image process

--- src/main.py
+++ src/main.py
@@ -5,11 +5,23 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 def process_image(image_input):
+
     input_image = Image.open(image_input)
     image_resize = input_image.resize((224, 224))
     image_array = image.img_to_array(image_resize)
     image_array = np.expand_dims(image_array, axis=0)
+
     return image_array
+
+
+def pca_process_image(image_one):
+
+    pca_input_image = Image.open(image_one)
+    pca_image_resize = pca_input_image.resize((224, 224))
+    pca_image_array = image.img_to_array(pca_image_resize)
+    pca_image_array = pca_image_array[:, :, 0]
+
+    return pca_image_array
 
 
 def cosine_similarity_calculation(embedding_one, embedding_two):
